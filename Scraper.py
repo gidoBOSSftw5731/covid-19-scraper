349f6ddcf210685a5c96ea0bf00608e7683df1db
gido5731 <celtictarot386@gmail.com>
2020-03-22 03:37:18 -0400

    add selenium and headless

--- Scraper.py
+++ Scraper.py
@@ -4,6 +4,16 @@ import requests
 import json
 import urllib.request
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import sys
+import os
+
+
+options = Options()
+options.add_argument("--headless")
+
+driver = webdriver.Firefox(executable_path=os.getcwd()+"/geckodriver",options=options)
 
 #Currently requires google auth
 #auth.authenticate_user()
@@ -21,7 +31,7 @@ from bs4 import BeautifulSoup
 
 total = 0
 
-################################################################################
+
 
 #Alabama
 url = urllib.request.urlopen("https://services7.arcgis.com/4RQmZZ0yaZkGR1zy/arcgis/rest/services/COV19_Public_Dashboard_ReadOnly/FeatureServer/0/query?f=json&where=CONFIRMED%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22CONFIRMED%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true")
@@ -31,7 +41,7 @@ AL = jc['features'][0]['attributes']['value']
 print(AL)
 total += AL
 
-################################################################################
+
 
 #Alaska
 url = "http://dhss.alaska.gov/dph/Epi/id/Pages/COVID-19/monitoring.aspx"
@@ -45,7 +55,14 @@ AK = cnt[0]
 print(AK)
 total += int(AK)
 
-################################################################################
+
+#Arizona
+#lol no
+
+
+#Arkansas
+
+
 
 #Output Handling
 #store = gc.open_by_url('https://docs.google.com/spreadsheets/d/19PpoExlTc7I4V-HpxvrqDGDrKuRND10Hm3hA_pJvnjw/edit?usp=sharing').sheet2
