f4891cd7bdfbec4ab98a676208ff8fc06a15b2e9
gido5731 <celtictarot386@gmail.com>
2020-03-24 17:48:42 -0400

    Add ToS for discord bot

--- arcgis_scraper/arcgis_scraper.go
+++ arcgis_scraper/arcgis_scraper.go
@@ -4,9 +4,10 @@ import (
 	"bytes"
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
-	"fmt"
+
 	"github.com/jinzhu/configor"
 
 	"github.com/gidoBOSSftw5731/log"
@@ -20,7 +21,6 @@ var config = struct {
 		Port     string `default:"5432"`
 		IP       string `default:"127.0.0.1"`
 	}
-	
 }{}
 
 type arcgis struct {
@@ -59,9 +59,8 @@ type Properties struct {
 
 var db *sql.DB
 
-
 const (
-	arcgisURL     = "https://opendata.arcgis.com/datasets/628578697fb24d8ea4c32fa0c5ae1843_0.geojson"
+	arcgisURL = "https://opendata.arcgis.com/datasets/628578697fb24d8ea4c32fa0c5ae1843_0.geojson"
 )
 
 func main() {
@@ -86,15 +85,14 @@ func main() {
 // looping Downloader is intended to run in the background, downloading the data from ArcGIS every 5 minutes
 // and adding any new entries to the database
 func loopingDownloader() {
-		form, err := downloadArcgis()
-		if err != nil {
-			log.Fatalln(err)
-		}
+	form, err := downloadArcgis()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-		
-		//var sqlCombined string
-		
-		for _, entry := range form.Featuress {
+	//var sqlCombined string
+
+	for _, entry := range form.Featuress {
 		/*	err = db.QueryRow("SELECT combined FROM records WHERE combined=$1", entry.Properties.OBJECTID).Scan(&sqlCombined)
 			switch {
 				case err == sql.ErrNoRows:
@@ -102,35 +100,34 @@ func loopingDownloader() {
 					log.Errorln(err)
 					continue
 				default:
-				
+
 			}*/
 
-			p := &entry.Properties
+		p := &entry.Properties
 
-			uTime, err := time.Parse(time.RFC3339, p.LastUpdate)
-			if err != nil {
-				log.Errorln(err)
-				continue
-			}
-			uTimeUnix := uTime.Unix()
+		uTime, err := time.Parse(time.RFC3339, p.LastUpdate)
+		if err != nil {
+			log.Errorln(err)
+			continue
+		}
+		uTimeUnix := uTime.Unix()
 
-			if (p.PeopleTested == nil) {
-				p.PeopleTested = 0
-			}
-
-			if (p.FIPS == "") {
-				p.FIPS = "0"
-			}
-
-			_, err = db.Exec("INSERT INTO records (country, state, county, unixtime, lat, long, deaths, confirmed, tests, recovered, fips, combined, incidentrate) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
-		p.CountryRegion, p.ProvinceState, p.County, uTimeUnix, p.Lat, p.Long, p.Deaths, p.Confirmed, p.PeopleTested, p.Recovered, p.FIPS, p.CombinedKey, p.IncidentRate)
-			if err != nil {
-					log.Errorf("Error: %v\nstruct: %v", err, p)
-					continue
-				}
+		if p.PeopleTested == nil {
+			p.PeopleTested = 0
 		}
 
-	
+		if p.FIPS == "" {
+			p.FIPS = "0"
+		}
+
+		_, err = db.Exec("INSERT INTO records (country, state, county, unixtime, lat, long, deaths, confirmed, tests, recovered, fips, combined, incidentrate) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
+			p.CountryRegion, p.ProvinceState, p.County, uTimeUnix, p.Lat, p.Long, p.Deaths, p.Confirmed, p.PeopleTested, p.Recovered, p.FIPS, p.CombinedKey, p.IncidentRate)
+		if err != nil {
+			log.Errorf("Error: %v\nstruct: %v", err, p)
+			continue
+		}
+	}
+
 }
 
 func downloadArcgis() (arcgis, error) {
@@ -146,7 +143,6 @@ func downloadArcgis() (arcgis, error) {
 	buf.ReadFrom(resp.Body)
 	jsonIn := buf.String()
 
-
 	err = json.Unmarshal([]byte(jsonIn), &form)
 	if err != nil {
 		return form, err
@@ -158,25 +154,25 @@ func mkDB() (*sql.DB, error) {
 	return sql.Open("postgres", fmt.Sprintf("user=%v password=%v dbname=covid19scraper host=%v port=%v",
 		config.DB.User, config.DB.Password, config.DB.IP, config.DB.Port))
 	/*
-create database covid19scraper;
-create user covid19scraper with encrypted password 'ThatsWhatICallInfected';
-CREATE TABLE records (
-country text,
-state text,
-county text,
-unixtime int,
-lat float,
-long float,
-deaths int,
-confirmed int,
-tests int,
-recovered int,
-fips int,
-combined text,
-incidentrate float,
-inserttime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
-GRANT ALL ON ALL TABLES IN SCHEMA public TO covid19scraper;
-create index idx_combined on records (combined);
+		create database covid19scraper;
+		create user covid19scraper with encrypted password 'ThatsWhatICallInfected';
+		CREATE TABLE records (
+		country text,
+		state text,
+		county text,
+		unixtime int,
+		lat float,
+		long float,
+		deaths int,
+		confirmed int,
+		tests int,
+		recovered int,
+		fips int,
+		combined text,
+		incidentrate float,
+		inserttime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+		);
+		GRANT ALL ON ALL TABLES IN SCHEMA public TO covid19scraper;
+		create index idx_combined on records (combined);
 	*/
 }
