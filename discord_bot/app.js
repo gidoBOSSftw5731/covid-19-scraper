d1b5f9bda96930f3122d7206be53fcd8bed8fe7a
Dr AlienTech <rdatch101@gmail.com>
2020-03-25 22:31:01 -0400

    lots of interesting things that may be broken XP

--- discord_bot/app.js
+++ discord_bot/app.js
@@ -12,58 +12,59 @@ admin.initializeApp({
 
 let db = admin.firestore();
 
-const states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia',
-    'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
-    'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
-    'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina',
-    'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+const states = [
+    'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA',
+    'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA',
+    'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND',
+    'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
+    'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'
+];
 
 client.on("ready", () => {
     console.log(`Client user tag: ${client.user.tag}!`);
 })
 
 client.on("message", msg => {
-    if (msg.content === "!signin") {
-        msg.reply(msg.author.id);
-        user(msg.author.id);
-    }
     
     if (!msg.content.startsWith("!") || msg.author.bot) return;
 
     const args = msg.content.slice(1).split(' ');
     const command = args.shift().toLowerCase();
 
-    if (command === "location") {
-        if (!args.length) {
-            return msg.reply("To use location command, please follow the paradigm:\n" +
-            "```!location <state> <county (optional)>```\n Note: At this moment, only the US is supported.");
-        } else if (!states.includes(args[0])) {
-            return msg.reply('Looks like you may have entered the parameters wrong!\n Please follow the paradigm:\n' +
-                "```!location <state> <county (optional)>```\n Note: At this moment, only the US is supported.");
-        } else {
-            location(msg.author.id, args, msg.channel);
-        }
+    switch (command) {
+        case "signup":
+            if (msg.content === "!signup") {
+                msg.reply(msg.author.id);
+                let userDoc = db.collection('users').doc(msg.author.id).set({
+                    id: msg.author.id
+                });
+            }
+            break;
+        case "location":
+            if (!args.length) {
+                return msg.reply("To use location command, please follow the paradigm:\n" +
+                    "```!location <state (abbreviation)> <county (optional)>```\n Note: At this moment, only the US is supported.");
+            } else if (!states.includes(args[0])) {
+                return msg.reply('Looks like you may have entered the parameters wrong!\n Please follow the paradigm:\n' +
+                    "```!location <state> <county (optional)>```\n Note: At this moment, only the US is supported.");
+            } else {
+                var o = "";
+                for (i = 0; i < args.length; i++) {
+                    if (i == 0) {
+                        let updateState = db.collection('users').doc(msg.author.id).update({ state: args[0] });
+                        o += "State: " + args[0];
+                    } else if (i == 1) {
+                        let updateCounty = db.collection('users').doc(msg.author.id).update({ county: args[1] });
+                        o += ", County: " + args[1];
+                    }
+                }
+                msg.reply(`Location added!\n${o}`);
+            }
+            break;
+        case "botinfo":
+            break;
+        default:
+            msg.reply('You must add a command for me to know what to do! Use !help to see a list of commands');
     }
 })
-
-function user(id) {
-    let userDoc = db.collection('users').doc(id).set({
-        id: id
-    });
-}
-
-function location(id, args, channel) {
-    var o = "";
-    for (i = 0; i < args.length; i++) {
-        if (i == 0) {
-            let updateState = db.collection('users').doc(id).update({ state: args[0] });
-            o += "State: " + args[0];
-        } else if (i == 1) {
-            let updateCounty = db.collection('users').doc(id).update({ county: args[1] });
-            o += ", County: " + args[1];
-        }
-    }
-    channel.send(`Location added!\n${o}`);
-}
-
 client.login(process.env.BOT_TOKEN)
--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +8,7 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.createNotification = functions.firestore.document('data/{state}/{county}/{docid}').onUpdate((change, context) => {
+    
+});
