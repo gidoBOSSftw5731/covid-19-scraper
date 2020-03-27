0983b9100b93d9e5a52648ce943c132118db24c8
gido5731 <celtictarot386@gmail.com>
2020-03-26 23:57:09 -0400

    working on FS ArcGIS live updates

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 const db = firebase.firestore();
+const AGFileURL = "https://opendata.arcgis.com/datasets/628578697fb24d8ea4c32fa0c5ae1843_0.geojson"
 
 exports.sendDM = functions.firestore.document('users/{userID}').onWrite((change, context) => {
     console.log('change triggered');
@@ -19,4 +20,16 @@ exports.sendDM = functions.firestore.document('users/{userID}').onWrite((change,
     })
 
     client.login(process.env.BOT_TOKEN)
+});
+
+exports.updateFSArcGIShttp = functions.https.onRequest((req, res) => {
+    let oldData = req.body.data
+
+    fetch(AGFileURL).then(res => res.buffer()).then(buffer => {
+        if (buffer == oldData) {
+            res.status(200).send("works!")
+        } else {
+            res.status(404).send("This means they were not the same")
+        }
+    })
 });
