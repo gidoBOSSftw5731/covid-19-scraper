bfb57be1c83245ed2125b46eb2ae857112f84c54
gido5731 <celtictarot386@gmail.com>
2020-04-02 01:57:04 -0400

    Add bot to list cases in golang

--- discord_bot_go/discord_bot_go.go
+++ discord_bot_go/discord_bot_go.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	pb "github.com/gidoBOSSftw5731/covid-19-scraper/apiListener/proto"
 	"github.com/gidoBOSSftw5731/log"
+	"google.golang.org/protobuf/proto"
 
 	"github.com/joho/godotenv"
 )
@@ -13,6 +20,7 @@ var (
 	botID         string
 	commandPrefix string
 	discordToken  string
+	apiURL        string
 )
 
 func main() {
@@ -22,6 +30,7 @@ func main() {
 	errCheck("error getting env stuff ", err)
 	discordToken = os.Getenv("BOT_TOKEN")
 	commandPrefix = os.Getenv("BOT_PREFIX")
+	apiURL = os.Getenv("API_URL")
 
 	discord, err := discordgo.New("Bot " + discordToken)
 	errCheck("error creating discord session", err)
@@ -61,4 +70,61 @@ func commandHandler(discord *discordgo.Session, message *discordgo.MessageCreate
 		return
 	}
 
+	if !(message.Content[:len(commandPrefix)] == commandPrefix) {
+		return
+	}
+
+	command := strings.Split(message.Content, commandPrefix)[1]
+	commandContents := strings.Split(message.Content, " ") // 0 = !command, 1 = first arg, etc
+	if len(commandContents) < 2 {
+		log.Errorln("didnt supply enough args")
+		//discord.ChannelMessageSend(message.ChannelID, "Error in formatting!")
+		return
+	}
+
+	switch strings.Split(command, " ")[0] {
+	case "cases":
+		state := commandContents[len(commandContents)-1]
+		county := strings.Title(strings.Join(commandContents[1:len(commandContents)-1], " "))
+		country := "US" // change this if we support more than just good ol' 'murica
+
+		queryURL := apiURL + "/currentinfo/" + country + "/" + state + "/" + county
+
+		if county == "" {
+			queryURL = queryURL[:len(queryURL)-1]
+		}
+
+		log.Tracef("QueryURL: %v", queryURL)
+
+		resp, err := http.Get(queryURL)
+		if err != nil {
+			log.Errorln(err)
+			return
+		}
+
+		defer resp.Body.Close()
+
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(resp.Body)
+
+		log.Tracef("Base64 data: %v", buf.String())
+
+		protoIn, err := base64.StdEncoding.DecodeString(buf.String())
+		if err != nil {
+			log.Errorln(err)
+			return
+		}
+
+		newAreaInfo := &pb.AreaInfo{}
+
+		err = proto.Unmarshal(protoIn, newAreaInfo)
+		if err != nil {
+			log.Errorln(err)
+		}
+
+		msgStr := fmt.Sprintf("The %v of %v has %v cases, %v deaths, has given %v tests, and has %v recoveries!",
+			newAreaInfo.Type, "foo", newAreaInfo.ConfirmedCases, newAreaInfo.Deaths, newAreaInfo.TestsGiven, newAreaInfo.Recoveries)
+		discord.ChannelMessageSend(message.ChannelID, msgStr)
+
+	}
 }
--- discord_bot_go/go.mod
+++ discord_bot_go/go.mod
@@ -4,6 +4,6 @@ go 1.13
 
 require (
 	github.com/bwmarrin/discordgo v0.20.2 // indirect
-	github.com/gidoBOSSftw5731/log v0.0.0-20190718204308-3ae037c6203f // indirect
+	github.com/gidoBOSSftw5731/covid-19-scraper/apiListener v0.0.0-20200402051635-3219733128ce // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
 )
