c835766129910f83aaa25c034fa46b99a2674700
Dr AlienTech <rdatch101@gmail.com>
2020-04-05 15:53:17 -0400

    housekeeping

--- public/caching.js
+++
@@ -1,9 +0,0 @@
-async function setCache({ state = null, county = null, data = null } = {}) {
-    if (county && data) {
-        var location = state + "_" + county;
-        localStorage.setItem(location, data);
-    } else if (state && data) {
-        localStorage.setItem(state, data);
-    }
-    return console.log("data saved to cache for later retrieval");
-}
