7403e220b755bbcb77e06f906c3cdb0e3ac1a653
Dr AlienTech <rdatch101@gmail.com>
2020-04-04 22:46:01 -0400

    semicolons!!!

--- public/statefromapi.js
+++ public/statefromapi.js
@@ -1,33 +1,34 @@
 function loadInStates() {
-    console.log("wheeeeee")
+    console.log("wheeeeee");
     var elem = document.getElementById("state");
-    country = "US"
-    var re = /[\w(\w )]+/
+    country = "US";
+    var re = /[\w(\w )]+/;
     var fragment = document.createDocumentFragment();
 
     doRequestWrap("https://buttstuff.ops-netman.net/liststates/" + country).then(function (text){
-        decoded = Buffer.from(text.toString(), 'base64')
+        decoded = Buffer.from(text.toString(), 'base64');
 
-        states = decoded.matchAll(re)
+        states = decoded.matchAll(re);
         
         states.forEach(function(state, index) {
             if (index == 0) {
-                continue
+                continue;
             }
-            console.log(state)
+            console.log(state);
 
-            opt = elem.createElement('option')
-            opt.innerHTML = state
-            opt.value = state
-            fragment.appendChild(opt)
+            opt = elem.createElement('option');
+            opt.innerHTML = state;
+            opt.value = state;
+            fragment.appendChild(opt);
         })
-        elem.appendChild(fragment)
-    })
+        elem.appendChild(fragment);
+    });
 };
 
 async function doRequestWrap(url) {
-    return await doRequest(url)
-}
+    return await doRequest(url);
+};
+
 function doRequest(url) {
     return new Promise(function (resolve, reject) {
         request(url, function (error, res, body) {
