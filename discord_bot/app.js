41779c883858d919a98308df1481992ead5d62dd
Dr AlienTech <alientechgamig@hotmail.com>
2020-04-19 15:31:30 -0400

    changes

--- discord_bot/app.js
+++ discord_bot/app.js
@@ -58,7 +58,7 @@ db.collection('env').doc('env').get().then(function (doc) {
 
     proxyClient.on("ready", function readysetgo() {
         client.channels.get(`Site Client user tag: ${proxyClient.user.id}!`);
-        proxyClient.user.setActivity("alone", { type: "Playing" });
+        proxyClient.user.setActivity("Washing hands", { type: 4 });
         return proxyClient.removeListener('on', readysetgo);
     });
 }).catch(function (err) {
